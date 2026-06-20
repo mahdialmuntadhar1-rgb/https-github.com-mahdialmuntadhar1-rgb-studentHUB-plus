784f78d39b6beedb13c23af09a2fddc2693dbfd1
Mahdi <mahdialmuntadhar1@gmail.com>
2026-06-20 18:46:14 +0300

    Restore official Campus Life design posts without fake stats

--- src/data/campusLifeMockPosts.ts
+++ src/data/campusLifeMockPosts.ts
@@ -1,28 +1,2 @@
-﻿export type CampusLifeMockPost = any;
-export type CampusLifePost = any;
-export type CampusPost = any;
-export type OfficialCampusLifePost = any;
-
-const emptyCampusLifePosts: any[] = [];
-
-export const officialJamiaatiDesignPosts = emptyCampusLifePosts;
-export const campusLifeMockPosts = emptyCampusLifePosts;
-export const campusLifeFeedItems = emptyCampusLifePosts;
-export const officialCampusLifePosts = emptyCampusLifePosts;
-export const mockCampusLifePosts = emptyCampusLifePosts;
-export const campusLifePosts = emptyCampusLifePosts;
-
-export function getCampusLifeMockPosts() {
-  return emptyCampusLifePosts;
-}
-
-export function generateCampusLifeMockPosts() {
-  return emptyCampusLifePosts;
-}
-
-export function getCampusLifePosts() {
-  return emptyCampusLifePosts;
-}
-
-export default emptyCampusLifePosts;
+﻿
 
