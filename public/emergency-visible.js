8ad31032bb393c9a57a3fe7e621246058fda537c
Mahdi <mahdialmuntadhar1@gmail.com>
2026-06-26 20:02:12 +0300

    Freeze working Talaba production after white blank fix

---
+++ public/emergency-visible.js
@@ -0,0 +1,5 @@
+﻿setTimeout(function(){
+  var root = document.getElementById("root");
+  var fb = document.getElementById("talaba-emergency-visible");
+  if (root && fb && root.children.length > 0) fb.style.display = "none";
+}, 2000);
